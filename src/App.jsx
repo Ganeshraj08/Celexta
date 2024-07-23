fd76b0b5f5f08bc6c0d703303f094a63720bade6
rahulj1011 <rahuljr2022@gmail.com>
2024-07-23 22:51:38 +0530

    added contact-us page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import About from "./pages/About";
 import Lecture from "./pages/Lecture";
 import Congrats from "./pages/Congrats";
 import Certificate from "./pages/Certificate";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -23,9 +24,10 @@ const App = () => {
             <Route path="/service" element={<Services />} />
             <Route path="/courses" element={<CoursesPage/>} />
             <Route path="/about-us" element={<About />} />
-            <Route path="/contact-us" element={<Home />} />
+            <Route path="/contact-us" element={<Contact />} />
             <Route path="/lecture" element={<Lecture />} />
             <Route path="/congrats" element={<Congrats />} />
+          
             <Route path="/certificate" element={<Certificate />} />
           </Routes>
         </main>
